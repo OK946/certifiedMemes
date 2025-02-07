59ade309919ba45b29409a0bb85cb1dc35349f78
OK946 <owen.kolbert24@student.cart.org>
2025-02-07 15:37:46 -0800

    Added additional settings modified by the certificates

--- script.js
+++ script.js
@@ -13,18 +13,24 @@ const swissCheese = new Object();
     swissCheese.awardImage = "/images/swissCheese.png"
     swissCheese.giver = "Chad (The Man) Brown";
     swissCheese.desc = "What even is that? Are you proud of this?"
+    swissCheese.job = "Just Trust Him"
+    swissCheese.jobPlace = "Please"
 // assigns an object that represents the swiss cheese certificate
 const cactusAward = new Object();
     cactusAward.awardName = "Cactus";
     cactusAward.awardImage = "/images/cactus.png"
     cactusAward.giver = "Will";
     cactusAward.desc = "You're a Cactus. That's Cool."
+    cactusAward.job = "Guy We Found"
+    cactusAward.jobPlace = "Outside Dept. of Food Science"
 // assigns an object that represents the cactus certificate
 const pearAward = new Object();
     pearAward.awardName = "Pear";
     pearAward.awardImage = "/images/pear.png"
     pearAward.giver = "John Johnson IIIVXX";
     pearAward.desc = "You're a Pear. Congrats!!"
+    pearAward.job = "Professional Taste Tester"
+    pearAward.jobPlace = "Department of Food Science"
 // assigns an object that represents the pear certificate
 entry.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -39,22 +45,33 @@ entry.addEventListener("submit", (e) => {
     for (i = 0; i < types.length; i++){
         if (types[i].checked){
             let type = types[i].id
+            // This assigns the checked button to a variable that is checked below.
             if (type == "swiss"){
+            // This checks which of the radio buttons is selected
                 document.getElementById('background').src = swissCheese.awardImage;
                 document.getElementById(`awardName`).textContent = swissCheese.awardName;
                 document.getElementById(`giver`).textContent = swissCheese.giver;
                 document.getElementById(`description`).textContent = swissCheese.desc;
+                document.getElementById(`job`).textContent = swissCheese.job;
+                document.getElementById(`jobPlace`).textContent = swissCheese.jobPlace;
+                // These edit objects in the certificate that change based on the type
                 // I know this repeats and I could've used a function, but I only realized that in the last 2 hours of the project and I needed to work on other things. So sue me.
             } else if (type =="cactus") {
                 document.getElementById('background').src = cactusAward.awardImage;
                 document.getElementById(`awardName`).textContent = cactusAward.awardName;
                 document.getElementById(`giver`).textContent = cactusAward.giver;
                 document.getElementById(`description`).textContent = cactusAward.desc;
+                document.getElementById(`job`).textContent = cactusAward.job;
+                document.getElementById(`jobPlace`).textContent = cactusAward.jobPlace;
+                // These edit objects in the certificate that change based on the type
             } else {
                 document.getElementById('background').src = pearAward.awardImage;
                 document.getElementById(`awardName`).textContent = pearAward.awardName;
                 document.getElementById(`giver`).textContent = pearAward.giver;
                 document.getElementById(`description`).textContent = pearAward.desc;
+                document.getElementById(`job`).textContent = pearAward.job;
+                document.getElementById(`jobPlace`).textContent = pearAward.jobPlace;
+                // These edit objects in the certificate that change based on the type
             }
         }
 
